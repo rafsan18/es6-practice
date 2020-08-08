f3b14275c383228bd95b7eb97c7663efc7562655
rafsan <rafsanamin18@gmail.com>
2020-08-08 14:33:52 +0600

    let const done

---
+++ letconst.js
@@ -0,0 +1,9 @@
+// constant is declared if the value of the variable is fixed.
+
+const numbers = [12, 34];
+numbers[1] = 67;
+numbers.push(45);
+console.log(numbers);
+
+// let is a scope variable.
+// when var is used in scope it can be leak so let is used in the for loop ( let i = 0;)
